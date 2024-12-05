485f937c24b1f12285d38e8b82a6ba10b0af72d5
bhavy1204 <bhavysarwa51@gmail.com>
2024-12-05 08:44:19 +0530

    Added teacher class

--- LMS.cpp
+++ LMS.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
+//class student
 class Student
 {
     int contactNo;
@@ -35,6 +36,45 @@ public:
         cout<<"Mother Name : "<<Mother_name;
         cout<<"Father Name : "<<Father_name;
         cout<<"Address : "<<address;
+    }
+};
+//Class Teacher
+class Teacher
+{
+    int contactNo;
+    string address;
+    string Mother_name;
+    string Father_name;
+public:
+    string Teach_name;
+    int rollno;
+    void Student_Detail(){
+        string Teach_name,Mother_name, father_name, address;
+        int rollno, contactNo;
+        cout<<"-------------------------------------------------------------"<<endl;
+        cout<<"Enter Teacher Name : ";
+        getline(cin,Teach_name);
+        cout<<"Enter ID : ";
+        cin>>rollno;
+        cout<<"Enter student contact : ";
+        cin>>contactNo;
+        cout<<"Enter Fathers name : ";
+        getline(cin,Father_name);
+        cout<<"Enter Mothers name : ";
+        getline(cin,Mother_name);
+        cout<<"Enter Address : ";
+        getline(cin,address);
+        cout<<"-------------------------------------------------------------"<<endl;
+    }
+    void show_teacher_detail(){
+        cout<<"--------------------------------------------------------------"<<endl;
+        cout<<"Name : "<<Teach_name;
+        cout<<"Name : "<<rollno;
+        cout<<"Conatct : "<<contactNo;
+        cout<<"Mother Name : "<<Mother_name;
+        cout<<"Father Name : "<<Father_name;
+        cout<<"Address : "<<address;
+        cout<<"---------------- ---------------------------------------------"<<endl;
     }
 };
 int main()
