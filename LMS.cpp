3374ffcddbd4269e17532b8e0e292fadbebeba0c
bhavy1204 <bhavysarwa51@gmail.com>
2024-12-23 18:39:48 +0530

    Updated courses menu

--- LMS.cpp
+++ LMS.cpp
@@ -15,7 +15,7 @@ public:
     int duration;
     void add_course()
     {
-        cout << "--------------------------------------------------------------" << endl;
+        cout << "-------------------------------------------------------" << endl;
         cout << "Enter Course Name : ";
         getline(cin, name);
         cout << "Enter course id : ";
@@ -24,16 +24,16 @@ public:
         cin >> max_marks;
         cout << "Enter Course duration(Hrs) : ";
         cin >> duration;
-        cout << "--------------------------------------------------------------" << endl;
+        cout << "---------------------------------------------------------" << endl;
     }
     void course_detail()
     {
-        cout << "------------------------------------------------------------" << endl;
+        cout << "-------------------------------------------------------" << endl;
         cout << "Name : " << name;
         cout << "id : " << id;
         cout << "maximim marks : " << max_marks;
         cout << "Durationn(Hrs): " << duration;
-        cout << "------------------------------------------------------------" << endl;
+        cout << "-------------------------------------------------------" << endl;
     }
 };
 // class student
@@ -126,13 +126,14 @@ int main()
 {
     system("cls");
     bool done = false;
-    int studentTop = -1,teacherTop=-1;
+    int studentTop = -1, teacherTop = -1;
     Student s[MAX];
     Teacher t1[MAX];
     int choice;
 
     do
     {
+        // Main menu
         cout << "------------------------------------------------------" << endl;
         cout << "1. students " << endl;
         cout << "2. teachers " << endl;
@@ -151,13 +152,14 @@ int main()
                 cout << "2.View all student details " << endl;
                 cout << "3.Add student(s)" << endl;
                 cout << "4.Update student detail " << endl;
-                cout << "5. Go back \n>> ";
+                cout << "5.View result " << endl;
+                cout << "6.View results of all " << endl;
+                cout << "7. Go back \n>> ";
                 cin >> sub_choice;
                 cout << "--------------------------------------------" << endl;
                 switch (sub_choice)
                 {
                 case 1:
-                {
                     int rn;
                     cout << "Enter roll no :- ";
                     cin >> rn;
@@ -173,21 +175,16 @@ int main()
                     if (found == false)
                         cout << "NOT FOUND IN RECORD" << endl;
                     break;
-                }
                 case 2:
                     for (i = 0; i <= studentTop; i++)
-                    {
                         s->show_student_detail();
-                    }
                     break;
                 case 3:
                     cin.ignore();
                     if (studentTop + 1 < MAX)
                         s[++studentTop].Student_Detail();
                     else
-                    {
                         cout << "Batch capacity reached! Cannot add more students." << endl;
-                    }
                     break;
                 case 4:
                 {
@@ -209,10 +206,29 @@ int main()
                         cout << "NOT FOUND IN RECORD" << endl;
                     break;
                 }
+                case 5:
+                    int rn;
+                    cout << "Enter roll no :- ";
+                    cin >> rn;
+                    bool found = false;
+                    for (i = 0; i < MAX; i++)
+                    {
+                        if (s[i].rollno == rn)
+                        {
+                            s[i].show_student_detail();
+                            found = true;
+                        }
+                    }
+                    if (found == false)
+                        cout << "NOT FOUND IN RECORD" << endl;
+                    break;
+                case 6:
+                    cout << "View result of all" <<endl;
+                    break;
                 default:
                     break;
                 }
-            } while (sub_choice != 5);
+            } while (sub_choice != 7);
             break;
 
         case 2:
@@ -244,15 +260,15 @@ int main()
                     break;
                 }
                 case 2:
-                    if (teacherTop==-1)
-                        cout<<"No teacher to show "<<endl;
+                    if (teacherTop == -1)
+                        cout << "No teacher to show " << endl;
                     else
                         for (i = 0; i <= teacherTop; i++)
                             t1[i].show_teacher_detail();
                     break;
                 case 3:
-                    if(teacherTop==MAX-1)
-                        cout<<"Memory full"<<endl;
+                    if (teacherTop == MAX - 1)
+                        cout << "Memory full" << endl;
                     else
                         t1[++teacherTop].teacher_Detail();
                     break;
